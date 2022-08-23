8372833d6c1a7004fc06d6299fcc51bdda76f862
presh-031 <akingbulugbe.dev@gmail.com>
2022-08-24 00:21:55 +0100

    trying to update score in dom

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 const gameItems = ['rock', 'paper', 'scissors'];
 let computerChoice = '';
+let score = Number(document.querySelector('.score').textContent);
+// console.log(score);
 
 function getComputerChoice() {
   const randomNum = Math.trunc(Math.random() * 3);
@@ -12,30 +14,40 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   // const result = `You Lose! ${playerSelection.toLowerCase()} beats ${computerSelection}`;
   let result = document.querySelector('.results');
+
   let finalPlayerSelection = playerSelection.toLowerCase();
   if (finalPlayerSelection === 'paper') {
     if (computerSelection === gameItems[0]) {
       result.textContent = `You Win! 🎉 ${finalPlayerSelection} beats ${computerSelection}`;
+      score += 1;
     } else if (computerSelection === gameItems[2]) {
       result.textContent = `You Lose!😖 ${computerSelection} beats ${finalPlayerSelection}`;
+      score -= 1;
     } else {
       result.textContent = `Draw! ⛔ ${computerSelection} = ${finalPlayerSelection}`;
+      score = score;
     }
   } else if (finalPlayerSelection === 'rock') {
     if (computerSelection === gameItems[2]) {
       result.textContent = `You Win!🎉 ${finalPlayerSelection} beats ${computerSelection}`;
+      score += 1;
     } else if (computerSelection === gameItems[1]) {
+      score -= 1;
       result.textContent = `You Lose!😖 ${computerSelection} beats ${finalPlayerSelection}`;
     } else {
       result.textContent = `Draw!⛔ ${computerSelection} = ${finalPlayerSelection}`;
+      score = score;
     }
   } else if (finalPlayerSelection === 'scissors') {
     if (computerSelection === gameItems[1]) {
       result.textContent = `You Win! ${finalPlayerSelection} beats ${computerSelection}`;
+      score += 1;
     } else if (computerSelection === gameItems[0]) {
       result.textContent = `You Lose! ${computerSelection} beats ${finalPlayerSelection}`;
+      score -= 1;
     } else {
       result.textContent = `Draw!⛔ ${computerSelection} = ${finalPlayerSelection}`;
+      score = score;
     }
   } else {
     result.textContent = `Invalid Selection. Choose either "rock", "paper" or "scissors".`;
@@ -43,9 +55,6 @@ function playRound(playerSelection, computerSelection) {
   return result;
 }
 function game() {
-  // for (let i = 0; i < 5; i++) {
-  //   console.log(playRound(prompt(), getComputerChoice()));
-  // }
   const btns = Array.from(document.querySelectorAll('button'));
   console.log(btns);
 
